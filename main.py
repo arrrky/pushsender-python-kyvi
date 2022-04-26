65e289dba37f4f4fb391eecc017be1ea38c7c9ee
arrrky <arkanesobaka@gmail.com>
2022-04-26 23:17:50 +0500

    MainWindow -> TabbedPanel, some .kv structure changes

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from kivy.app import App
 
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
-from kivy.uix.textinput import TextInput
+from kivy.uix.tabbedpanel import TabbedPanel
 
 
 class PushSenderApp(App):
     def build(self):
-        return MainTab()
+        return MainWindow()
 
 
-class MainTab(Widget):
+class MainWindow(TabbedPanel):
+    pass
+
+
+class ApplicationsNamesDropDown(DropDown):
     pass
 
 
